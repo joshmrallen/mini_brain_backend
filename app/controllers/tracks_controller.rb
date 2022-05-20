4af71e1a6181e801457075144d9e761f97a67b1c
Josh Allen <40782086+joshmrallen@users.noreply.github.com>
2022-05-20 17:04:46 -0400

    added test responses to all actions

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,10 +1,58 @@
 class TracksController < ApplicationController
   def focus
+    response = {
+      :message => "Successful focus response!",
+      :mental_state => "focus",
+      :tracks => [
+        {
+          :filename => "Focus Track 1"
+        },
+        {
+          :filename => "Focus Track 2"
+        },
+        {
+          :filename => "Focus Track 3"
+        }
+      ]
+    }
+    render json: response
   end
 
   def relax
+    response = {
+      :message => "Successful response!",
+      :mental_state => "relax",
+      :tracks => [
+        {
+          :filename => "Relax Track 1"
+        },
+        {
+          :filename => "Relax Track 2"
+        },
+        {
+          :filename => "Relax Track 3"
+        }
+      ]
+    }
+    render json: response
   end
 
   def sleep
+    response = {
+      :message => "Successful response!",
+      :mental_state => "sleep",
+      :tracks => [
+        {
+          :filename => "Sleep Track 1",
+        },
+        {
+          :filename => "Sleep Track 2",
+        },
+        {
+          :filename => "Sleep Track 3"
+        }
+      ]
+    }
+    render json: response
   end
 end
