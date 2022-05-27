f02e591621cb8af74227a15de81f0466f4c2f663
Josh Allen <40782086+joshmrallen@users.noreply.github.com>
2022-05-27 14:39:07 -0400

    refactored and added private methods

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,80 +1,34 @@
 class TracksController < ApplicationController
+
   def focus
-    track_1_data = Base64.encode64(File.read("#{Rails.root}/app/assets/focus/aspen_migration.mp3"))
-    track_2_data = Base64.encode64(File.read("#{Rails.root}/app/assets/focus/cyan.mp3"))
-    track_3_data = Base64.encode64(File.read("#{Rails.root}/app/assets/focus/treasure_map.mp3"))
-    response = {
-      :message => "Successful focus response!",
-      :mental_state => "focus",
-      :tracks => [
-        {
-          :filename => "aspen_migration.mp3",
-          :data => track_1_data
-        },
-        {
-          :filename => "cyan.mp3",
-          :data => track_2_data
-        },
-        {
-          :filename => "treasure_map.mp3",
-          :data => track_3_data
-        }
-      ],
-      :status => 200
-    }
-    render :json => response
+    stream_it
   end
 
   def relax
-    track_1_data = Base64.encode64(File.read("#{Rails.root}/app/assets/relax/bathed_in_neon.mp3"))
-    track_2_data = Base64.encode64(File.read("#{Rails.root}/app/assets/relax/low_tide.mp3"))
-    track_3_data = Base64.encode64(File.read("#{Rails.root}/app/assets/relax/vapor.mp3"))
-    response = {
-      :message => "Successful response!",
-      :mental_state => "relax",
-      :tracks => [
-        {
-          :filename => "bathed_in_neon.mp3",
-          :data => track_1_data
-        },
-        {
-          :filename => "low_tide.mp3",
-          :data => track_2_data
-        },
-        {
-          :filename => "vapor.mp3",
-          :data => track_3_data
-        }
-      ],
-      :status => 200
-    }
-    render :json => response
+    stream_it
   end
 
   def sleep
-    track_1_data = Base64.encode64(File.read("#{Rails.root}/app/assets/sleep/dark_moon.mp3"))
-    track_2_data = Base64.encode64(File.read("#{Rails.root}/app/assets/sleep/lunaris.mp3"))
-    track_3_data = Base64.encode64(File.read("#{Rails.root}/app/assets/sleep/moonflower.mp3"))
-
-    response = {
-      :message => "Successful response!",
-      :mental_state => "sleep",
-      :tracks => [
-        {
-          :filename => "dark_moon.mp3",
-          :data => track_1_data
-        },
-        {
-          :filename => "lunaris.mp3",
-          :data => track_2_data
-        },
-        {
-          :filename => "moonflower.mp3",
-          :data => track_3_data
-        }
-      ],
-      :status => 200
-    }
-    render :json => response
+    stream_it
   end
+
+  private
+
+  def track_params
+    params.require(:track).permit(:id)
+  end
+
+  def stream_it
+    directory = "#{Rails.root}/app/assets/#{action_name}"
+    index = 0
+    file_hash = Hash.new
+
+    Dir.each_child(directory) do |file|
+      file_hash[index] = {:path => "#{directory}/#{file}", :filename => file}
+      index += 1
+    end
+
+    send_file file_hash[track_params][:path], type: "audio/mpeg", filename: file_hash[track_params][:filename]
+  end
+
 end
