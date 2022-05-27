258bb2a52659e88ce2a43d1c601a6b1c9353de21
Josh Allen <40782086+joshmrallen@users.noreply.github.com>
2022-05-27 14:54:25 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'tracks/focus'
-  get 'tracks/relax'
-  get 'tracks/sleep'
+  get 'tracks/focus/:id' => 'tracks#focus'
+  get 'tracks/relax' => 'tracks#relax'
+  get 'tracks/sleep' => 'tracks#sleep'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
